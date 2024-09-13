966209c3e29141132ad134e4bcdab31f067f8276
natorgas <123809166+natorgas@users.noreply.github.com>
2024-09-13 19:48:07 +0100

    update

--- NeuronalesNetz/Load_PTH_calculation_model.py
+++ NeuronalesNetz/Load_PTH_calculation_model.py
@@ -2,26 +2,29 @@ import tensorflow as tf
 import numpy as np
 import os
 
+def swish(x):
+    return x * tf.keras.activations.sigmoid(x)
+
+# Register the custom activation function
+tf.keras.utils.get_custom_objects().update({'swish': tf.keras.layers.Activation(swish)})
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
+saved_model_dir = os.path.join(dir_path, "PTH_calculation_model.keras")
 
-saved_model_dir = os.path.join(dir_path, "PTH_calculation_model_stable_is_40.keras")
-
-model = tf.keras.models.load_model(saved_model_dir)
+# Load the model
+model = tf.keras.models.load_model(saved_model_dir, custom_objects={'swish': tf.keras.layers.Activation(swish)})
 
 alpha = float(input("Alpha: "))
 beta = float(input("Beta: "))
 
-print(f"Vorhersage für Alpha = {alpha}° und Beta = {beta}°")
+print(f"Prediction for Alpha = {alpha}° and Beta = {beta}°")
 input_data = np.array([[alpha, beta]])
 pred = float(model.predict(input_data)[0][0])
 
 if pred >= 35:
-    print("Vorhersage PTH: 40s")
-    print("-> Winkelpaar ist stabil")
-
+    print("Prediction PTH: 40s")
+    print("-> Angle pair is stable")
 elif 30 < pred < 35:
-    print("Vorhersage PTH: 30s")
-
+    print("Prediction PTH: 30s")
 else:
-    print(f"Vorhersage PTH: {round(pred, 2)}s")
-
+    print(f"Prediction PTH: {round(pred, 2)}s")
